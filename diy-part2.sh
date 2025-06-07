f89bd0d8f5ca8de46779e5894049fe5da87dfbe8
Haris131 <41672454+Haris131@users.noreply.github.com>
2025-06-07 13:58:39 +0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,4 +17,6 @@ sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_gener
 sed -i 's/luci-theme-bootstrap/luci-theme-material/g' feeds/luci/collections/luci/Makefile
 
 # Patch OverClock
+wget https://github.com/Haris131/linux-msm/commit/bfbe1ebeb1df2bcfd8a04addde787f1abc00d914.patch -O target/linux/msm89xx/patches-6.1/0096-overclock-msm8916.patch
 wget https://github.com/Haris131/linux-msm/commit/bfbe1ebeb1df2bcfd8a04addde787f1abc00d914.patch -O target/linux/msm89xx/patches-6.12/0096-overclock-msm8916.patch
+wget https://github.com/Haris131/linux-msm/commit/bfbe1ebeb1df2bcfd8a04addde787f1abc00d914.patch -O target/linux/msm89xx/patches-6.16/0096-overclock-msm8916.patch
