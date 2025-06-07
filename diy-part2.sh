257c4b1f1b4e380bf24bd8ef3da912c313de295c
Haris131 <41672454+Haris131@users.noreply.github.com>
2025-06-07 13:46:19 +0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,10 +11,10 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-material/g' feeds/luci/collections/luci/Makefile
 
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+# Patch OverClock
+wget https://github.com/Haris131/linux-msm/commit/bfbe1ebeb1df2bcfd8a04addde787f1abc00d914.patch -O target/linux/msm89xx/patches-6.12/0096-overclock-msm8916.patch
