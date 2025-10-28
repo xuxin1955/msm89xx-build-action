10474f0713f2589f00d556c52c05e034257b166e
xuxin1955 <73999603+xuxin1955@users.noreply.github.com>
2025-10-28 13:43:26 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,32 +17,32 @@ sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_gener
 sed -i 's/luci-theme-material/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Patch OverClock
-wget https://github.com/Haris131/uz801v3-kernel/commit/fabdd0494c48f7c86ebf63f96e2a8c213ecdb96c.patch -O target/linux/msm89xx/patches-6.1/0096-overclock-msm8916.patch
-wget https://github.com/Haris131/uz801v3-kernel/commit/fabdd0494c48f7c86ebf63f96e2a8c213ecdb96c.patch -O target/linux/msm89xx/patches-6.6/0096-overclock-msm8916.patch
-wget https://github.com/Haris131/uz801v3-kernel/commit/fabdd0494c48f7c86ebf63f96e2a8c213ecdb96c.patch -O target/linux/msm89xx/patches-6.12/0096-overclock-msm8916.patch
+# wget https://github.com/Haris131/uz801v3-kernel/commit/fabdd0494c48f7c86ebf63f96e2a8c213ecdb96c.patch -O target/linux/msm89xx/patches-6.1/0096-overclock-msm8916.patch
+# wget https://github.com/Haris131/uz801v3-kernel/commit/fabdd0494c48f7c86ebf63f96e2a8c213ecdb96c.patch -O target/linux/msm89xx/patches-6.6/0096-overclock-msm8916.patch
+# wget https://github.com/Haris131/uz801v3-kernel/commit/fabdd0494c48f7c86ebf63f96e2a8c213ecdb96c.patch -O target/linux/msm89xx/patches-6.12/0096-overclock-msm8916.patch
 
 # Patch Fix led name
-wget https://github.com/Haris131/uz801v3-kernel/commit/55fad55a0f18517d02d66003b5ec70975d46accb.patch -O target/linux/msm89xx/patches-6.1/0097-fix-led-msm8916.patch
-wget https://github.com/Haris131/uz801v3-kernel/commit/55fad55a0f18517d02d66003b5ec70975d46accb.patch -O target/linux/msm89xx/patches-6.6/0097-fix-led-msm8916.patch
-wget https://github.com/Haris131/uz801v3-kernel/commit/55fad55a0f18517d02d66003b5ec70975d46accb.patch -O target/linux/msm89xx/patches-6.12/0097-fix-led-msm8916.patch
+# wget https://github.com/Haris131/uz801v3-kernel/commit/55fad55a0f18517d02d66003b5ec70975d46accb.patch -O target/linux/msm89xx/patches-6.1/0097-fix-led-msm8916.patch
+# wget https://github.com/Haris131/uz801v3-kernel/commit/55fad55a0f18517d02d66003b5ec70975d46accb.patch -O target/linux/msm89xx/patches-6.6/0097-fix-led-msm8916.patch
+# wget https://github.com/Haris131/uz801v3-kernel/commit/55fad55a0f18517d02d66003b5ec70975d46accb.patch -O target/linux/msm89xx/patches-6.12/0097-fix-led-msm8916.patch
 
-cat <<EOF >> target/linux/msm89xx/config-6.1
+# cat <<EOF >> target/linux/msm89xx/config-6.1
 
-CONFIG_BT_QCOMSMD=m
-CONFIG_QCOM_IPA=m
-EOF
+# CONFIG_BT_QCOMSMD=m
+# CONFIG_QCOM_IPA=m
+# EOF
 
-cat <<EOF >> target/linux/msm89xx/config-6.6
+# cat <<EOF >> target/linux/msm89xx/config-6.6
 
-CONFIG_BT_QCOMSMD=m
-CONFIG_QCOM_IPA=m
-EOF
+# CONFIG_BT_QCOMSMD=m
+# CONFIG_QCOM_IPA=m
+# EOF
 
-cat <<EOF >> target/linux/msm89xx/config-6.12
+# cat <<EOF >> target/linux/msm89xx/config-6.12
 
-CONFIG_BT_QCOMSMD=m
-CONFIG_QCOM_IPA=m
-EOF
+# CONFIG_BT_QCOMSMD=m
+# CONFIG_QCOM_IPA=m
+# EOF
 
 # HelmiWrt packages
-git clone --depth=1 https://github.com/Haris131/helmiwrt-packages package/community
+# git clone --depth=1 https://github.com/Haris131/helmiwrt-packages package/community
